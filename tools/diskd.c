6f2205580a76719c9fa3d717c8447b62c5bae9d2
Hideo Yamauchi <renayama19661014@ybb.ne.jp>
2022-04-04 13:26:52 +0900

    Mid: diskd: support for pacemaker 2.0.x.

--- tools/diskd.c
+++ tools/diskd.c
@@ -41,9 +41,8 @@
 #include <time.h>
 #include <string.h>
 
-#include <crm/attrd.h>
+#include <attrd_internal.h>
 #include <crm/common/mainloop.h>
-
 #ifdef HAVE_GETOPT_H
 #  include <getopt.h>
 #endif
@@ -92,11 +91,11 @@ int pagesize = 0;
 void *ptr = NULL;
 void *buf;
 
-#if PACEMAKER_GE_1113
-int attr_options = attrd_opt_none;
-#else
-gboolean attr_options = FALSE;
-#endif
+//#if PACEMAKER_GE_1113
+int attr_options = pcmk__node_attr_none;
+//#else
+//gboolean attr_options = FALSE;
+//#endif
 
 #if GLIB_CHECK_VERSION(2, 32, 0)
 GMutex diskd_mutex;
@@ -119,7 +118,8 @@ static void diskd_thread_timer_variable_free(void);
 static void diskd_thread_condsend(void);
 static void diskd_thread_timer_end(void);
 void send_update(void);
-void crm_make_daemon(const char *name, gboolean daemonize, const char *pidfile);
+//void crm_make_daemon(const char *name, gboolean daemonize, const char *pidfile);
+void pcmk__daemonize(const char *name, const char *pidfile);
 
 static void
 diskd_shutdown(int nsig)
@@ -136,7 +136,7 @@ diskd_shutdown(int nsig)
 	if (mainloop != NULL && g_main_is_running(mainloop)) {
 		g_main_quit(mainloop);
 	} else {
-		crm_exit(EX_OK);
+		crm_exit(0);
 	}
 }
 
@@ -748,8 +748,13 @@ main(int argc, char **argv)
 		rc = oneshot();
 		crm_exit(rc);
 	}
-
-	crm_make_daemon(crm_system_name, daemonize, pid_file);
+#if 1
+        if (daemonize) {
+	    pcmk__daemonize(crm_system_name, pid_file);
+        }
+#else
+        crm_make_daemon(crm_system_name, daemonize, pid_file);
+#endif
 	diskd_thread_timer_init();
 
 	if ( wflag ) {	/* writer */
@@ -800,20 +805,20 @@ send_update(void)
 {
 	int rc;
 
-#if ATTRD_UPDATE_BOTH
+//#if ATTRD_UPDATE_BOTH
 	static gboolean boFirst = TRUE;
-#else
-	static gboolean boFirst = FALSE;
-#endif
+//#else
+//	static gboolean boFirst = FALSE;
+//#endif
 
 	if (boFirst) {
-	    rc = attrd_update_delegate(NULL, 'B', NULL, diskd_attr,
+	    rc = pcmk__node_attr_request(NULL, 'B', NULL, diskd_attr,
 		diskcheck_value, attr_section, attr_set, attr_dampen, NULL, attr_options);
 	    if (rc == pcmk_ok) {
 			boFirst = FALSE;
 	    }
 	} else {
-	    rc = attrd_update_delegate(NULL, 'U', NULL, diskd_attr,
+	    rc = pcmk__node_attr_request(NULL, 'U', NULL, diskd_attr,
 		diskcheck_value, attr_section, attr_set, attr_dampen, NULL, attr_options);
 	}
 
